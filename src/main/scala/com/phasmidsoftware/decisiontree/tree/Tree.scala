fb3ab5569985cafc7ae04c0d7cfe668a3e68307c
Robin Hillyard <scalaprof@gmail.com>
2020-05-20 18:25:15 -0400

    Generalize dfs and bfs methods in Tree
    Tree: added trait Visitor;

--- src/main/scala/com/phasmidsoftware/decisiontree/tree/Tree.scala
+++ src/main/scala/com/phasmidsoftware/decisiontree/tree/Tree.scala
@@ -48,6 +48,19 @@ object Node {
  */
 case class Tree[T](key: T, children: Seq[Node[T]] = Nil) extends Node[T]
 
+trait Visitor[T, V] {
+  def visit(v: V, t: T): V
+}
+
+object Visitor {
+
+  trait QueueVisitor[T] extends Visitor[T, Queue[T]] {
+    def visit(v: Queue[T], t: T): Queue[T] = v.enqueue(t)
+  }
+
+  implicit def queueVisitor[T]: QueueVisitor[T] = new QueueVisitor[T] {}
+}
+
 object Tree {
 
   /**
@@ -59,94 +72,92 @@ object Tree {
    */
   implicit class TreeOps[T](node: Node[T]) {
 
-    type Item = Either[Node[T], T]
-
     /**
-     * Method to get the "preOrder" of a tree.
+     * Method to get the "preOrder" of a tree via depth-first search.
      * In preOrder, any node will have its value recorded BEFORE processing its children.
      *
      * @param p a predicate that determines whether a subtree with its value t will be processed at all.
      * @return an Iterable of T values in the order they were visited.
      */
-    def preOrder(p: T => Boolean = always): Iterable[T] = {
-      @tailrec
-      def inner(queue: Queue[T], nodes: Seq[Node[T]]): Queue[T] = nodes match {
-        case Nil => queue
-        case tn :: tns if tn.filter(p) => inner(queue.enqueue(tn.key), tn.children ++ tns)
-        case _ :: tns => inner(queue, tns)
-      }
+    def preOrder(p: T => Boolean = always): Iterable[T] = traversePre(Queue.empty[T], p, Seq(node))
 
-      inner(Queue.empty, Seq(node))
-    }
+    type Item = Either[Node[T], T]
 
     /**
-     * Method to get the "inOrder" of a tree.
+     * Method to get the "inOrder" of a tree via depth-first search.
      * In inOrder, any node will have its value recorded AFTER processing its first child,
      * but BEFORE processing the rest of its children.
      *
      * @param p a predicate that determines whether a subtree with its value t will be processed at all.
      * @return an Iterable of T values in the order they were visited.
      */
-    def inOrder(p: T => Boolean = always): Iterable[T] = {
-      @tailrec
-      def inner(queue: Queue[T], work: Seq[Item]): Queue[T] = work match {
-        case Nil => queue
-        case w :: ws => w match {
-          case Left(tn) =>
-            if (tn.filter(p))
-              tn.children match {
-                case Nil =>
-                  inner(queue, Right(tn.key) +: ws)
-                case first :: rest =>
-                  inner(queue, Left(first) +: Right(tn.key) +: (rest.map(Left(_)) ++ ws))
-              }
-            else inner(queue, ws)
-          case Right(t) =>
-            inner(queue.enqueue(t), ws)
-        }
-      }
-
-      inner(Queue.empty, Seq(Left(node)))
-    }
+    def inOrder(p: T => Boolean = always): Iterable[T] = traverse(Queue.empty[T], p)(Seq(Left(node)))
 
     /**
-     * Method to get the "postOrder" of a tree.
+     * Method to get the "postOrder" of a tree via depth-first search.
      * In postOrder, any node will have its value recorded AFTER processing all children,
      *
      * @param p a predicate that determines whether a subtree with its value t will be processed at all.
      * @return an Iterable of T values in the proper order.
      */
-    def postOrder(p: T => Boolean = always): Iterable[T] = {
-      @tailrec
-      def inner(queue: Queue[T], work: Seq[Item]): Queue[T] = work match {
-        case Nil => queue
-        case w :: ws => w match {
-          case Left(tn) =>
-            if (tn.filter(p))
-              inner(queue, tn.children.map(Left(_)) ++ (Right(tn.key) +: ws))
-            else
-              inner(queue, ws)
-          case Right(t) =>
-            inner(queue.enqueue(t), ws)
-        }
-      }
+    def postOrder(p: T => Boolean = always): Iterable[T] = traverse2(Queue.empty[T], p)(Seq(Left(node)))
 
-      inner(Queue.empty, Seq(Left(node)))
+    /**
+     * Method to get the breadth-first order of a tree.
+     *
+     * @param p a predicate that determines whether a subtree with its value t will be processed at all.
+     */
+    def bfs(p: T => Boolean = always): Iterable[T] = bfs(Queue.empty[T], p)(Queue(node))
+
+    @tailrec
+    private final def traversePre[V](visitor: V, p: T => Boolean, nodes: Seq[Node[T]])(implicit tVv: Visitor[T, V]): V = nodes match {
+      case Nil => visitor
+      case tn :: tns if tn.filter(p) => traversePre(tVv.visit(visitor, tn.key), p, tn.children ++ tns)
+      case _ :: tns => traversePre(visitor, p, tns)
     }
 
-    def bfs(p: T => Boolean = always): Iterable[T] = {
-      @tailrec
-      def inner(queue1: Queue[T], queue2: Queue[Node[T]]): Queue[T] = {
-        queue2.dequeueOption match {
-          case None => queue1
-          case Some((tn, q)) =>
-            if (tn.filter(p))
-              inner(queue1.enqueue(tn.key), tn.children.foldLeft(q)(_.enqueue(_)))
-            else inner(queue1, q)
-        }
+    // CONSIDER merging traverse and traverse1
+    @tailrec
+    private final def traverse[V](visitor: V, p: T => Boolean)(work: Seq[Item])(implicit tVv: Visitor[T, V]): V = work match {
+      case Nil => visitor
+      case w :: ws => w match {
+        case Left(tn) =>
+          if (tn.filter(p))
+            tn.children match {
+              case Nil =>
+                traverse(visitor, p)(Right(tn.key) +: ws)
+              case first :: rest =>
+                traverse(visitor, p)(Left(first) +: Right(tn.key) +: (rest.map(Left(_)) ++ ws))
+            }
+          else traverse(visitor, p)(ws)
+        case Right(t) =>
+          traverse(tVv.visit(visitor, t), p)(ws)
       }
+    }
 
-      inner(Queue.empty, Queue(node))
+    @tailrec
+    private final def traverse2[V](visitor: V, p: T => Boolean)(work: Seq[Item])(implicit tVv: Visitor[T, V]): V = work match {
+      case Nil => visitor
+      case w :: ws => w match {
+        case Left(tn) =>
+          if (tn.filter(p))
+            traverse2(visitor, p)(tn.children.map(Left(_)) ++ (Right(tn.key) +: ws))
+          else
+            traverse2(visitor, p)(ws)
+        case Right(t) =>
+          traverse2(tVv.visit(visitor, t), p)(ws)
+      }
+    }
+
+    @tailrec
+    private final def bfs[V](visitor: V, p: T => Boolean)(queue2: Queue[Node[T]])(implicit tVv: Visitor[T, V]): V = {
+      queue2.dequeueOption match {
+        case None => visitor
+        case Some((tn, q)) =>
+          if (tn.filter(p))
+            bfs(tVv.visit(visitor, tn.key), p)(tn.children.foldLeft(q)(_.enqueue(_)))
+          else bfs(visitor, p)(q)
+      }
     }
   }
 
